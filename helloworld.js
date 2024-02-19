2511d31db9ca8d8bb1f87046ad67a1a3ad2bbd1c
Kingsley <kingsley.huang@mycare.org.nz>
2024-02-19 18:58:00 +1300

    just commit

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,13 @@
 console.log('hellow world!!!');
+
+function sum(a, b) {
+    console.log(a + b);
+    return "Done"
+}
+
+function sumReturn(a, b) {
+  return a + b;
+}
+
+console.log(sum(5, 1));
+// console.log(sumReturn(5, 1));
